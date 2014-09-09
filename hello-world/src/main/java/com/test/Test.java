318f3cc784caa665f707c653f331e6824edd4d94
Upesh Kulatunge <ukulatunge@gmail.com>
2014-09-09 12:26:33 +0530

    add test class from anypoint studio-1

---
+++ hello-world/src/main/java/com/test/Test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.test;
+
+/**
+ * @author upesh
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Anypoint-1");
+
+	}
+
+}
